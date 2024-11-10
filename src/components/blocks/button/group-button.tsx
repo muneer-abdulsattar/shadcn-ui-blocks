1eff1da651613bd35a08bd76dce35bbc2ec30ba7
Akash Moradiya <akashmoradiya3444@gmail.com>
2024-11-10 11:24:26 +0530

    refactor: group button

--- src/components/blocks/button/group-button.tsx
+++ src/components/blocks/button/group-button.tsx
@@ -3,14 +3,29 @@ import { Button } from "@/components/ui/button";
 const GroupButtonDemo = () => (
   <div className="flex items-center gap-2 flex-wrap">
     <div>
-      <Button className="rounded-r-none">Left</Button>
-      <Button className="rounded-none">Middle</Button>
-      <Button className="rounded-l-none">Right</Button>
+      <Button className="rounded-none first:rounded-l-md last:rounded-r-md">
+        Left
+      </Button>
+      <Button className="rounded-none first:rounded-l-md last:rounded-r-md">
+        Middle
+      </Button>
+      <Button className="rounded-none first:rounded-l-md last:rounded-r-md">
+        Middle
+      </Button>
+      <Button className="rounded-none first:rounded-l-md last:rounded-r-md">
+        Right
+      </Button>
     </div>
     <div className="divide-x divide-muted-foreground">
-      <Button className="rounded-r-none">Left</Button>
-      <Button className="rounded-none">Middle</Button>
-      <Button className="rounded-l-none">Right</Button>
+      <Button className="rounded-none first:rounded-l-md last:rounded-r-md">
+        Left
+      </Button>
+      <Button className="rounded-none first:rounded-l-md last:rounded-r-md">
+        Middle
+      </Button>
+      <Button className="rounded-none first:rounded-l-md last:rounded-r-md">
+        Right
+      </Button>
     </div>
   </div>
 );
--- src/lib/file.ts
+++ src/lib/file.ts
@@ -2,12 +2,17 @@ import path from "path";
 import fs from "fs";
 
 export const getNumberOfFilesInsideDirectory = (directoryPath: string) => {
-  const directoryFullPath = path.join(process.cwd(), directoryPath);
+  try {
+    const directoryFullPath = path.join(process.cwd(), directoryPath);
 
-  const files = fs.readdirSync(directoryFullPath);
-  const fileCount = files.filter((file) =>
-    fs.statSync(path.join(directoryFullPath, file)).isFile()
-  ).length;
+    const files = fs.readdirSync(directoryFullPath);
+    const fileCount = files.filter((file) =>
+      fs.statSync(path.join(directoryFullPath, file)).isFile()
+    ).length;
 
-  return fileCount;
+    return fileCount;
+  } catch (error) {
+    console.error("error :", error);
+    return 0;
+  }
 };
