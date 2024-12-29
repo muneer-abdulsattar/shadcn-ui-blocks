69d7a347508da46f4cbc579cec56790b07677629
Akash Moradiya <akashmoradiya3444@gmail.com>
2024-12-29 11:36:44 +0530

    fix(blocks): logos blocks responsiveness

--- src/blocks/logos-05/page.tsx
+++ src/blocks/logos-05/page.tsx
@@ -9,9 +9,12 @@ const Logos05Page = () => {
         <p className="text-center text-xl font-medium">
           More than 2.2 million companies worldwide already trust us
         </p>
-        <div className="mt-16 grid grid-cols-4 gap-6">
+        <div className="mt-16 grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 px-6">
           {logos.map((Logo, i) => (
-            <div key={i} className="bg-muted border p-10 rounded">
+            <div
+              key={i}
+              className="bg-muted border py-4 sm:p-10 rounded flex items-center justify-center"
+            >
               <Logo />
             </div>
           ))}
