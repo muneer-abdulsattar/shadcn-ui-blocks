1a9de084226fcb0ee60f79a445a8b9115fcd22ba
Akash Moradiya <akashmoradiya3444@gmail.com>
2024-12-29 14:50:54 +0530

    refactor(seo): add lastModified time to the sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,11 +3,14 @@ import { components } from "@/description/app-sidebar";
 import { MetadataRoute } from "next";
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const lastModified = new Date();
+
   const componentsSitemap: MetadataRoute.Sitemap = components.map(
     ({ url }) => ({
       url: `https://shadcn-ui-blocks.akashmoradiya.com${url}`,
       changeFrequency: "daily",
       priority: 1,
+      lastModified,
     })
   );
   const blocksCategorySitemap: MetadataRoute.Sitemap = blockCategories.map(
@@ -15,27 +18,28 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
       url: `https://shadcn-ui-blocks.akashmoradiya.com/blocks/categories/${name}`,
       changeFrequency: "daily",
       priority: 1,
+      lastModified,
     })
   );
 
   return [
     {
       url: "https://shadcn-ui-blocks.akashmoradiya.com",
-      lastModified: new Date(),
       changeFrequency: "monthly",
       priority: 1,
+      lastModified,
     },
     {
       url: "https://shadcn-ui-blocks.akashmoradiya.com/components/introduction",
-      lastModified: new Date(),
       changeFrequency: "monthly",
       priority: 0.8,
+      lastModified,
     },
     {
       url: "https://shadcn-ui-blocks.akashmoradiya.com/blocks",
-      lastModified: new Date(),
       changeFrequency: "daily",
       priority: 1,
+      lastModified,
     },
     ...componentsSitemap,
     ...blocksCategorySitemap,
