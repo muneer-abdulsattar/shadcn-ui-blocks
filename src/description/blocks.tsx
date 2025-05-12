2919fa7376b0c618a6ac6640f6b05c639ffca4b0
muneerabdulsattar <muneerabdulsattar@gmail.com>
2025-05-12 13:58:46 +0300

    Save

--- src/description/blocks.tsx
+++ src/description/blocks.tsx
@@ -3,21 +3,21 @@ import { DesktopIcon, MobileIcon } from "@radix-ui/react-icons";
 import { TabletIcon } from "lucide-react";
 
 export const blockScreens = [
-  {
-    name: BlockScreenSize.mobile,
-    icon: MobileIcon,
-    size: 30,
-  },
-  {
-    name: BlockScreenSize.tablet,
-    icon: TabletIcon,
-    size: 70,
-  },
-  {
-    name: BlockScreenSize.desktop,
-    icon: DesktopIcon,
-    size: 100,
-  },
+	{
+		name: BlockScreenSize.mobile,
+		icon: MobileIcon,
+		size: 30,
+	},
+	{
+		name: BlockScreenSize.tablet,
+		icon: TabletIcon,
+		size: 70,
+	},
+	{
+		name: BlockScreenSize.desktop,
+		icon: DesktopIcon,
+		size: 100,
+	},
 ];
 
 export const BLOCK_SCREENSHOT_WIDTH = 1366;
